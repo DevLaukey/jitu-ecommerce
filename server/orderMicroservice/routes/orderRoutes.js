a2c9ce61a7a9a16d9398ec7ff8501445924326cd
DevLaukey <devlaukeymwaura@gmail.com>
2022-07-22 15:09:46 +0300

    orders fixed

--- server/orderMicroservice/routes/orderRoutes.js
+++ server/orderMicroservice/routes/orderRoutes.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { getOrders, createOrder } = require("../controllers/orderController");
-const userAuth = require("../../middlewares/authMiddleware");
 
-router.get("/orders", userAuth.isAuthenticated, getOrders);
-router.post("/add-order", userAuth.isAuthenticated, createOrder);
+router.get("/orders", getOrders);
+router.post("/add-order", createOrder);
 
 module.exports = router;
